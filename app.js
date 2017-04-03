9f881df0d4f74c36528f255bf2fb0e20cf4c198d
Austin Peng <austin@Austins-MBP.home>
2017-04-02 22:58:08 -0400

    minor

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
 var app = angular.module("app", ["firebase"]);
 
-$("#mentor-req").click(function() {
-	$(".overlay").fadeIn(500);
-	$("#mentor-req-form").fadeIn(500);
-	console.log("hi")
-});
-
-$("#sendRequest").click(function() {
-	angular.element($(".main")).scope().sendRequest();
-	alert("Your request has been submitted! A mentor should be on his/her way to your table shortly.")
-	$(".overlay").fadeOut(500);
-	$(".alert").fadeOut(500);
-});
-
 $("#newMsg").keypress(function (event) {
  var key = event.which;
  if (key == 13) {
@@ -37,10 +24,10 @@ $(".alert .close-button").click(function() {
 $(".changeUsername").click(function() {
 	var u = "";
 	while (u == "" || u.toUpperCase() == "HACKMHS")  {
-		if (u.toUpperCase() == "HACKMHS") {
+		if (u.toUpperCase() == "GRAUER") {
 			alert("Sorry, that username is reserved. Please choose another.");
 		}
-		var u = prompt("What do you want your new username to be?");
+		var u = prompt("What would you like your username to be?");
 	}
 	angular.element($(".main")).scope().changeUsername(u);
 })
--- mc.js
+++ mc.js
@@ -1,8 +1,6 @@
 app.controller('MainController', function($scope, $firebaseArray) {
 
-	var requests = new Firebase("//hackmhs.firebaseio.com/requests");
 	var chat = new Firebase("https://the-ultimate-grauer-guid-c3743.firebaseio.com/");
-	$scope.requests = $firebaseArray(requests);
 	$scope.chat = $firebaseArray(chat);
 	$scope.username = "";
 	$scope.alertMessage = "";
@@ -48,45 +46,10 @@ app.controller('MainController', function($scope, $firebaseArray) {
 		});
 	}
 
-	$scope.sendRequest = function() {
-		var r = new Object();
-		r.name = $("#mentor-req-form .name").val();
-		r.teamName = $("#mentor-req-form .teamName").val();
-		r.tableNumber = $("#mentor-req-form .tableNumber").val();
-		r.details = $("#mentor-req-form .details").val();
-		r.timestamp = $scope.getCurrentTime();
-
-		$scope.requests.$add(r);
-		$("#mentor-req-form .name").val("");
-		$("#mentor-req-form .teamName").val("");
-		$("#mentor-req-form .tableNumber").val("");
-		$("#mentor-req-form .details").val("");
-	}
-
 	$scope.addAlert = function(alert) {
 		$scope.$apply(function() {
 			$scope.alertMessage = alert;
 		})
 	}
-
-	$scope.getCurrentTime = function() {
-		var d = new Date();
-		var hour = d.getHours();
-		var min = "0" + d.getMinutes();
-		min = min.substring(min.length - 2, min.length);
-		if (hour == 0) {
-			$scope.currentTime = "12:" + min + " AM";
-			return $scope.currentTime;
-		}
-		else if (d.getHours() > 12) {
-			$scope.currentTime = (hour - 12) + ":" + min + " PM";
-			return $scope.currentTime;
-		}
-		else {
-			$scope.currentTime = hour + ":" + min;
-			return $scope.currentTime;
-		}
-	}
-
 	
 });
