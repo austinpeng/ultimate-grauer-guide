9d51d05a48dfbbbdcd9f9df2e6d1daac09d7a770
Austin Peng <austin@Austins-MBP.home>
2017-04-06 00:47:50 -0400

    homework

--- app.js
+++ app.js
@@ -31,3 +31,19 @@ $(".changeUsername").click(function() {
 	}
 	angular.element($(".main")).scope().changeUsername(u);
 })
+
+var request = require('request')
+var fs = require('fs')
+var url = 'http://tesseract.projectnaptha.com/img/eng_bw.png'
+var filename = 'pic.png'
+ 
+var writeFileStream = fs.createWriteStream(filename)
+ 
+request(url).pipe(writeFileStream).on('close', function() {
+  console.log(url, 'saved to', filename)
+})
+
+ Tesseract.recognize(myImage)
+         .progress(function  (p) { alert('progress', p)    })
+         .then(function (result) { alert('result', result) })
+        
