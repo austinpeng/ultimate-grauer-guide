b73e41920393ee165e0dbf4f136eba45dd4eee9d
Austin Peng <austin@Austins-MBP.home>
2017-04-08 22:20:52 -0400

    formatting

--- now.js
+++
@@ -1,7 +0,0 @@
-app.controller('NowController', function($scope) {
-
-	$scope.tweet = "Head to http://bit.ly/hackmhs-demo\n for the demo order! After you finish lunch, start gathering your things and heading up to the auditorium.\n No food is allowed in the auditorium.";
-
-	// ??
-	
-});
